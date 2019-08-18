03e0fe126c282e4646179b107554aaad7c9a168b
ChrisCogs <ccogs@yahoo.com>
2019-08-18 13:47:04 -0400

    changed carousel to collabsible

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,11 @@
+// $(document).ready(function(){
+//     $('#gallery-carousel').carousel();
+//     $('.carousel.carousel-slider').carousel({
+//       fullWidth: true,
+//       indicators: true
+//     });
+//   });
+
 $(document).ready(function(){
-    $('#gallery-carousel').carousel();
-    $('.carousel.carousel-slider').carousel({
-      fullWidth: true,
-      indicators: true
-    });
-  });
+  $('.collapsible').collapsible();
+});
